2b90fc5320b849c53cfc8adda8d90cb2d7cf185b
OmarMesqq <omarmsqt@gmail.com>
2024-10-17 21:10:01 -0300

    chore: :recycle: ditch compose to use constraint layout

--- android/app/src/main/java/com/ingresso/challenge/view/MainActivity.kt
+++ android/app/src/main/java/com/ingresso/challenge/view/MainActivity.kt
@@ -1,47 +1,12 @@
 package com.ingresso.challenge.view
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ingresso.challenge.ui.theme.IngressoChallengeTheme
+import androidx.appcompat.app.AppCompatActivity
+import com.ingresso.challenge.R
 
-class MainActivity : ComponentActivity() {
+class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            IngressoChallengeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IngressoChallengeTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
