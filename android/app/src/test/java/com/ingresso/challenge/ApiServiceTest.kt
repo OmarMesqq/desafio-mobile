f1ef686b03a97b2ddaf8b02f0e8fe65c193590eb
OmarMesqq <omarmsqt@gmail.com>
2024-10-17 21:28:08 -0300

    fix: :bug: fix theme exception on startup

---
+++ android/app/src/test/java/com/ingresso/challenge/ApiServiceTest.kt
@@ -0,0 +1,17 @@
+package com.ingresso.challenge
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ApiServiceTest {
+    @Test
+    fun handle_no_internet() {
+        assertEquals(3, 2 + 2)
+    }
+
+    @Test
+    fun handle_bad_response() {
+        assertEquals(3, 2 + 2)
+    }
+}
