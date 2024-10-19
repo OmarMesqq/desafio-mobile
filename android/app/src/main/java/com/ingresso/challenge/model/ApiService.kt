a17d41dab42393c0618238eab0491f73f006e1f3
OmarMesqq <omarmsqt@gmail.com>
2024-10-19 17:16:25 -0300

    docs: :memo: add comments to ViewModelFactory

--- android/app/src/main/java/com/ingresso/challenge/model/ApiService.kt
+++ android/app/src/main/java/com/ingresso/challenge/model/ApiService.kt
@@ -1,7 +1,5 @@
 package com.ingresso.challenge.model
 
-import android.util.Log
-import com.ingresso.challenge.RetrofitClient
 import retrofit2.Response
 import retrofit2.http.GET
 
--- android/app/src/main/java/com/ingresso/challenge/viewmodel/ViewModelFactory.kt
+++ android/app/src/main/java/com/ingresso/challenge/viewmodel/ViewModelFactory.kt
@@ -4,11 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.ingresso.challenge.model.MovieRepository
 
+// Cria uma MovieViewModel recebendo o repositório via param para melhor coesão
 class ViewModelFactory(private val repository: MovieRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        // faz reflection para saber com segurança se a classe é compatível com MovieViewModel
+        // isso garante que temos a ViewModel esperada e não tenhamos erros em runtime
         if (modelClass.isAssignableFrom(MovieViewModel::class.java)) {
             return MovieViewModel(repository) as T
         }
-        throw IllegalArgumentException("Unknown ViewModel class")
+        throw IllegalArgumentException("ViewModelFactory.classNotAssignableFrom.MovieViewModel")
     }
 }
